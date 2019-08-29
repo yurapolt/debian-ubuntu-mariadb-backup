0092450d781f16f2e7ad0b7ece40cedcc4b091de
Yura Poltoratskiy <y.poltoratskiy@gmail.com>
2019-08-29 14:21:38 +0300

    switched backup owner from root to backup user

--- backup-mysql.sh
+++ backup-mysql.sh
@@ -3,7 +3,7 @@
 export LC_ALL=C
 
 days_of_backups=3  # Must be less than 7
-backup_owner="root"
+backup_owner="backup"
 parent_dir="/backup/mariadb"
 defaults_file="/etc/mysql/backup.cnf"
 todays_dir="${parent_dir}/$(date +%a)"
